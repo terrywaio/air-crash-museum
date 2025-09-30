a4d657b0546d41813845aae76f82c66b46a9af7e
terrywaio <terrywai7114@gmail.com>
2025-09-30 17:33:49 +0800

    Add all Air Crash Museum website files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/air-crash-museum',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
